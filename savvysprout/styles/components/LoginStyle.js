a6088a169c48627eaacdf809f7472a3b85587fc2
khairyo <ryu.minatozaki8@gmail.com>
2023-08-26 16:04:37 +0800

    css

--- savvysprout/styles/components/LoginStyle.js
+++ savvysprout/styles/components/LoginStyle.js
@@ -5,38 +5,42 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: "#fff",
       alignItems: "center",
-      justifyContent: "center",
+      justifyContent: "flex-start", // Start from the top
+      paddingTop: '23%', // Add paddingTop to push content down
     },
     image: {
-      marginBottom: 100,
+      marginBottom: 10,
       width: 200,
-      height:250,
+      height: 250,
     },
     inputView: {
-      backgroundColor: "#FFC0CB",
       borderRadius: 30,
-      width: "70%",
+      borderWidth: 4, // Add border
+      borderColor: "#ffcc15", // Yellow border color
+      backgroundColor: "white", // White background color
+      width: "75%",
       height: 45,
       marginBottom: 20,
       alignItems: "center",
     },
     TextInput: {
       height: 50,
+      width: '80%',
       flex: 1,
       padding: 10,
+      backgroundColor: "white",
     },
     forgot_button: {
       height: 30,
-      marginBottom: 30,
     },
     loginBtn: {
-      width: "80%",
+      width: "75%",
       borderRadius: 25,
       height: 50,
       alignItems: "center",
       justifyContent: "center",
-      marginTop: 40,
-      backgroundColor: "#FF1493",
+      marginTop: 20,
+      backgroundColor: "#ffcc15",
     },
   });
 
