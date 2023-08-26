f95c4ac91b51516195607a8ac3ab79797ffdde38
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-08-26 17:38:46 +0800

    hello

--- savvysprout/styles/components/HomeStyle.js
+++ savvysprout/styles/components/HomeStyle.js
@@ -7,6 +7,29 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
       },
+    
+    titleText: {
+      fontSize: 22,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      paddingVertical: 20,
+    },
+    cardStyle: {
+      width: '75%',
+      height: '45%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      borderRadius: 7,
+    },
+    cardTitleStyle: {
+      color: '#fff',
+      fontSize: 24,
+    },
+    swipeText: {
+      fontSize: 18,
+      textAlign: 'center',
+    },
 })
 
 export default styles
