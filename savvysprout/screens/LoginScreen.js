0a1fdd06980a92a2003f6241f13f9c34936ff199
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-08-26 16:45:37 +0800

    hi

--- savvysprout/screens/LoginScreen.js
+++ savvysprout/screens/LoginScreen.js
@@ -1,44 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {Text,View,Image,TextInput,Button,TouchableOpacity,} from "react-native";
+import {Text,View,Image,TextInput,Button,TouchableOpacity, KeyboardAvoidingView,} from "react-native";
 import styles from '../styles/components/LoginStyle';
 
-const LoginScreen = ({ navigation }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
+const LoginScreen = ({navigation}) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <View style={[styles.container]}>
-      <Image style={styles.image} source={require('../assets/log2.jpg')} />
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("../assets/log2.jpg")} /> 
       <StatusBar style="auto" />
-
-      {/* Email or Username input */}
       <View style={styles.inputView}>
         <TextInput
-          style={[styles.TextInput, styles.emailOrUsernameInput]}
+          style={styles.TextInput}
           placeholder="Email or Username"
           placeholderTextColor="#003f5c"
           onChangeText={(username) => setUsername(username)}
-        /> 
+        />
       </View> 
       <View style={styles.inputView}>
         <TextInput
-          style={[styles.TextInput, styles.passwordInput]}
+          style={styles.TextInput}
           placeholder="Password"
           placeholderTextColor="#003f5c"
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
-        />
-      </View>
-
+        /> 
+      </View> 
       <TouchableOpacity>
         <Text style={styles.forgot_button}>Forgot Password?</Text> 
       </TouchableOpacity> 
-      <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Register')}>
+      <TouchableOpacity style={styles.signupBtn} onPress={() => navigation.navigate('Register')}>
+        <Text style={styles.signupText}>Sign Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Home')}>
         <Text style={styles.loginText}>LOGIN</Text> 
       </TouchableOpacity>
     </View> 
   );
-};
+}
 
-export { LoginScreen as LoginScreen };
+export { LoginScreen as LoginScreen} ;
