b6b42431afcf0940a588eb4be04f154c47bff204
jsamaze <111722374+jsamaze@users.noreply.github.com>
2023-08-26 15:40:01 +0800

    Update HomeScreen.js

--- savvysprout/screens/HomeScreen.js
+++ savvysprout/screens/HomeScreen.js
@@ -15,33 +15,45 @@ const Users = [
     { id: "5", uri: require('../assets/5.jpg') },
   ]
 
+
+
 function SwipePage() {
+
+     renderUsers = () => {
+        return Users.map((item, i) => {
+           return (
+            <Animated.View 
+            key={i}
+            style={{
+            height: SCREEN_HEIGHT - 300,
+            width: SCREEN_WIDTH,
+            padding: 10
+            }}
+            >
+               <Image
+                 style={{
+                   flex: 1,
+                   height: null,
+                   width: null,
+                   resizeMode: "cover",
+                   borderRadius: 20
+                 }}
+                 source={item.uri}
+               />
+             </Animated.View>
+           );
+        });
+     };
+
     return (
-<View style={{ flex: 1 }}>
-        <View style={{ height: 60 }} />
         <View style={{ flex: 1 }}>
-          <Animated.View
-            style={[
-              {
-                height: SCREEN_HEIGHT - 300,
-                width: SCREEN_WIDTH,
-                padding: 10
-              }
-            ]}
-          >
-            <Image
-              style={{
-                flex: 1,
-                height: null,
-                width: null,
-                resizeMode: "cover",
-                borderRadius: 20
-              }}
-              source={Users[0].uri}
-            />
-          </Animated.View>
+        <View style={{ height: 60 }}>
         </View>
-        <View style={{ height: 60 }} />
+        <View style={{ flex: 1 }}>
+          {this.renderUsers()}
+        </View>
+        <View style={{ height: 60 }}>
+        </View>
       </View>
     );
   }
