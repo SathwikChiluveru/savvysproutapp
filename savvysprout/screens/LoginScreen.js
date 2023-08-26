690eb90b2d318155deec86db5a829fe27b733ea7
Jihoon_Song <69960711+HelloTech69@users.noreply.github.com>
2023-08-26 14:53:11 +0800

    Seperate Styles js

--- savvysprout/screens/LoginScreen.js
+++ savvysprout/screens/LoginScreen.js
@@ -1,14 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  Button,
-  TouchableOpacity,
-} from "react-native";
+import {Text,View,Image,TextInput,Button,TouchableOpacity,} from "react-native";
+import styles from '../styles/components/LoginStyle';
+
 
 const LoginScreen = ({navigation}) => {
   const [username, setUsername] = useState("");
@@ -43,43 +37,5 @@ const LoginScreen = ({navigation}) => {
     </View> 
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    marginBottom: 40,
-  },
-  inputView: {
-    backgroundColor: "#FFC0CB",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
-  },
-});
 
 export { LoginScreen as LoginScreen} ;
